f9b65d915a216302bf92ea1977d4ea18482828a9
Agustin <vissani1997@gmail.com>
2020-10-08 14:41:26 -0300

    Modificaciones con base de datos, agregue la incrementacion de id en clientes y proveedores

--- Base de datos/ConsultaProveedores.sql
+++ Base de datos/ConsultaProveedores.sql
@@ -2,12 +2,12 @@ use Martinica_Decoraciones
 
 /*
 INSERT INTO Proveedores
-VALUES (1,'Proveedor1','Apellido',1111111,'Domicilio',11-11111111-1),
- (2,'Proveedor2','Apellido2',2222222,'Domicilio2',22-2222222-2),
- (3,'Proveedor3','Apellido3',3333333,'Domicilio3',33-3333333-3),
- (4,'Proveedor3','Apellido3',4444444,'Domicilio3',4-4444444-4);
- */
+VALUES ('Proveedor1','Apellido',1111111,'Domicilio',11-11111111-1),
+ ('Proveedor2','Apellido2',2222222,'Domicilio2',22-2222222-2),
+ ('Proveedor3','Apellido3',3333333,'Domicilio3',33-3333333-3),
+ ('Proveedor3','Apellido3',4444444,'Domicilio3',4-4444444-4);
 
+ */
  --select * from Proveedores
 
 
--- Base de datos/Crear tablas.sql
+++ Base de datos/Crear tablas.sql
@@ -1,22 +1,21 @@
  use Martinica_Decoraciones
-
  /*
-  CREATE TABLE Clientes(Código_Cliente int not null, 
+ 
+  CREATE TABLE Clientes(Código_Cliente int not null identity(1, 1) primary key, 
  DNI int not null, 
  Nombre varchar(30) NOT NULL,
  Apellido varchar(30) NOT NULL,
  Teléfono varchar(20) NOT NULL,
- Domicilio varchar(30) NOT NULL,
- PRIMARY KEY ( Código_Cliente,DNI) ); 
-
-  CREATE TABLE Proveedores( Código_Proveedor int not null, 
+ Domicilio varchar(30) NOT NULL);
+ 
+  CREATE TABLE Proveedores( Código_Proveedor int not null identity(1, 1) primary key,
 -- Código_Cliente INT CONSTRAINT FK_Código_Cliente FOREIGN KEY(Código_Cliente) REFERENCES dbo.Clientes(Código_Cliente), 
  Nombre varchar(30) NOT NULL,
  Apellido varchar(30) NOT NULL,
  Teléfono varchar(20) NOT NULL,
  Domicilio varchar(30) NOT NULL,
- CUIT varchar(30) NOT NULL,
- PRIMARY KEY (Código_Proveedor) );
+ CUIT varchar(30) NOT NULL );
+
 
  CREATE TABLE Usuarios( idUsuario int not null, 
  Usuario varchar(50) NOT NULL,
@@ -24,4 +23,5 @@
  PRIMARY KEY (idUsuario) );
  */
 
- SELECT * FROM Usuarios where Usuario='Usuario' and Contraseña='12345';
+
+-- SELECT * FROM Usuarios where Usuario='Usuario' and Contraseña='1234';
